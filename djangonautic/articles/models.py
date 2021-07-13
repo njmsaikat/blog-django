af5423c903d7753e4433864f9e85885b6e757319
Saikat Roy <njmsaikat@gmail.com>
2021-07-13 06:31:56 +0600

    images added

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -9,7 +9,7 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    # thumb
+    thumb = models.ImageField(default="default.png", blank=True)
     # author
 
     def __str__(self):
--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import url, include
 from django.contrib import admin
@@ -28,3 +30,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
