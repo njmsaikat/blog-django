c9539b8eaee31b260d6957d555bba646c6afaafb
Saikat Roy <njmsaikat@gmail.com>
2021-07-13 06:12:31 +0600

    dynamic link go through

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,4 +1,6 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .models import Article
 
@@ -8,3 +10,10 @@ from .models import Article
 def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, "articles/article_list.html", {'articles_to_show': articles})
+
+
+def article_detail(request, slug):
+    # return HttpResponse(slug)
+    single_article_detail = Article.objects.get(slug=slug)
+
+    return render(request, 'articles/article_detail.html', {'article': single_article_detail})
